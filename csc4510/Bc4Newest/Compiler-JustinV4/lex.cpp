e11d19d929898684d81c536c11c4c2fde839a295
Cole Burkhart <cburkhart@linus-public.highpoint.edu>
2019-04-23 20:59:35 -0400

    Clean up

--- csc4510/Bc4Newest/Compiler-JustinV4/lex.cpp
+++ csc4510/Bc4Newest/Compiler-JustinV4/lex.cpp
@@ -1,3 +1,11 @@
+/*
+Authors: Cole Burkhart, Jacky Shi, Justin Weiss 
+Date: 4/23/2019
+Class: CSC 4510-01
+BC Version 4 - Recursive descent parser
+Uses grammar rules to parser statement that are entered in from the keyboard or input redirection
+
+*/
 #include "general.h"
 #include <iostream>
 #include <stdio.h>
@@ -5,6 +13,7 @@
 #include "symbolTable.h"
 #include <string.h>
 
+//Global symbol tables
 Symbol_table table;
 Symbol_ptr symbol_ptr;
 
@@ -14,7 +23,9 @@ using namespace std;
 int main ()
 {
 
+   //sets the inital program string to nothing
    program = "";
+   //index is -1
    curr_index = -1;
    inwhile = false;
 
@@ -35,7 +46,8 @@ int main ()
 
 /*****************************************************/
 /* lookup - a function to lookup operators and parentheses
- and return the token */
+ and return the token.
+Sets the nextToken and returns it */
 int lookup(char ch) {
    switch (ch) {
    case '(':
@@ -92,6 +104,7 @@ int lookup(char ch) {
      addChar();
      nextToken = LESSTHEN;
      break;
+
    case '>':
      addChar();
      nextToken = GREATERTHEN;
@@ -113,16 +126,20 @@ int lookup(char ch) {
 /* addChar - a function to add nextChar to lexeme */
 void addChar()
 {
-
- if (lexLen <= 98) {
- lexeme[lexLen++] = nextChar;
- lexeme[lexLen] = '\0';
- } else
- printf("Error - lexeme is too long \n");
+   if (lexLen <= 98) {
+      lexeme[lexLen++] = nextChar;
+      lexeme[lexLen] = '\0';
+   } else
+      printf("Error - lexeme is too long \n");
 }
 
-//Ge string
+//Get string
+//Buffers the input and resets the input to all for the while statement
+//to recheck the condition
+//returns a character
 char getString(){
+   
+   //checks if it is in a while statement
    if(inwhile == true){
       char temp = program[string_index];
       string_index++;
@@ -136,8 +153,6 @@ char getString(){
 }
 
 
-
-
 /*****************************************************/
 /* getChar - a function to get the next character of
  input and determine its character class
@@ -147,7 +162,8 @@ char getString(){
 */
 void getChar()
 {
- //need to chnage to all io redirection
+
+ //calls the get string function that we created to buffer the input
  if ((nextChar = getString()) != EOF) {
     if (isalpha(nextChar))
        charClass = LETTER;
@@ -157,11 +173,12 @@ void getChar()
        charClass = NEWLINE;
     else if(nextChar == '#')
        charClass = COMMENT;
-   else
-    charClass = OPERATOR;
- } else
- charClass = EOF;
+    else
+       charClass = OPERATOR;
+ }  else
+       charClass = EOF;
 }
+
 /*****************************************************/
 /* getNonBlank - remove white space characters.
  call getChar until it returns a non-whitespace
@@ -170,15 +187,17 @@ void getChar()
 */
 void getNonBlank()
 {
-  //WILL NEED TO FIX TO NOT REMOVE NEWLINES
-  //ALSO NEED THE NEWLINE TO BE THE END OF LINE SYMBOL
-      while (isspace(nextChar) && nextChar != '\n')
-        getChar();
+    //Gets the nextChar if it is not a space
+    while (isspace(nextChar) && nextChar != '\n')
+       getChar();
 
 }
 /*****************************************************/
 /* lex - a simple lexical analyzer for arithmetic
- expressions */
+ expressions 
+ Gets the next lexeme and token
+ Calls the other functions above to do this
+ Returns an integer*/
 int lex()
 {
     lexLen = 0;
@@ -196,34 +215,60 @@ int lex()
         addChar();
         getChar();
       }
-      //NEED TO STORE THE IDENT IN THE SYMBOL TABLE
-      //LOOKUP TO SEE IF IT IS THERE IF NOT THEN INSERT
-      //IF IT IS THEN ADJUST VALUE
 
-      //put in table
-
+    //Checks if the lexeme is dump 
+    //if it is then it sets nextToken as DUMP
     if(strcmp(lexeme,"dump") == 0){
        nextToken = DUMP;
     }
+
+    //Checks if the lexeme is quit 
+    //if it is then it sets nextToken as QUIT
     else if (strcmp(lexeme,"quit") == 0){
        nextToken = QUIT;
+
+    //Checks if the lexeme is If 
+    //if it is then it sets nextToken as IF
     } else if (strcmp(lexeme, "if") == 0) {
         nextToken = IF;
+
+    //Checks if the lexeme is then 
+    //if it is then it sets nextToken as THEN
     } else if (strcmp(lexeme, "then") == 0) {
       nextToken = THEN;
+
+    //Checks if the lexeme is else 
+    //if it is then it sets nextToken as ELSE
     } else if (strcmp(lexeme, "else") == 0) {
       nextToken = ELSE;
+
+    //Checks if the lexeme is fi
+    //if it is then it sets nextToken as FI
     } else if (strcmp(lexeme, "fi") == 0) {
       nextToken = FI;
+
+    //Checks if the lexeme is while 
+    //if it is then it sets nextToken as WHILE
     } else if (strcmp(lexeme, "while") == 0) {
       nextToken = WHILE;
+
+    //Checks if the lexeme is do 
+    //if it is then it sets nextToken as D)
     } else if (strcmp(lexeme, "do") == 0) {
       nextToken = DO;
+
+    //Checks if the lexeme is done 
+    //if it is then it sets nextToken as DONE
     } else if (strcmp(lexeme, "done") == 0) {
       nextToken = DONE;
+
+    //Checks if the lexeme is print 
+    //if it is then it sets nextToken as print
     } else if (strcmp(lexeme, "print") == 0) {
       nextToken = PRINT;
     }
+  
+    //Else the lexeme is an identifier and it is put in the symbol table
     else
     {
        nextToken = IDENT;
@@ -235,36 +280,38 @@ int lex()
  /* Parse integer literals - once you find the first
  digit, read and add digits to lexeme. */
     case DIGIT:
-    addChar();
-    getChar();
-    while (charClass == DIGIT) {
-    addChar();
-    getChar();
-    }
-    nextToken = INT_LIT;
+       addChar();
+       getChar();
+
+       while (charClass == DIGIT) {
+          addChar();
+          getChar();
+       }
+       nextToken = INT_LIT;
     break;
 
     /* Parentheses and operators */
     case OPERATOR:
-    /* Call lookup to identify the type of operator */
-    lookup(nextChar);
-    getChar();
-    break;
-    /* EOF */
-    //EOF WILL NEED TO BE NEWLINE SINCE THAT IS OUR NEW LINE SEPERATOR
-    case EOF:
-    nextToken = EOF;
-    lexeme[0] = 'E';
-    lexeme[1] = 'O';
-    lexeme[2] = 'F';
-    lexeme[3] = '\0';
+       /* Call lookup to identify the type of operator */
+       lookup(nextChar);
+       getChar();
     break;
 
-    //comment
+    /* EOF */
+    case EOF:
+       nextToken = EOF;
+       lexeme[0] = 'E';
+       lexeme[1] = 'O';
+       lexeme[2] = 'F';
+       lexeme[3] = '\0';
+    break;
+
+    //comment case
+    //process the entire line as a comment
     case COMMENT:
-      do{
-         getChar();
-      } while (charClass != NEWLINE);
+       do{
+          getChar();
+       } while (charClass != NEWLINE);
     break;
 
 
